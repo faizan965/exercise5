79fd7050a7ee72b9138b86307e7857f0da6e958d
faizan965 <140983174+faizan965@users.noreply.github.com>
2023-08-04 03:33:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//store a name famous person and message
+let famous_person = "Muhammad Ali Jinnah";
+let message = "Failure is word unkown to me";
+console.log(`famous Person: ${famous_person}`);
+console.log(`Message: "${message}"`);
---
+++ index.ts
@@ -0,0 +1,9 @@
+//store a name famous person and message
+
+let famous_person : string = "Muhammad Ali Jinnah";
+
+let message : string = "Failure is word unkown to me";
+
+console.log(`famous Person: ${famous_person}`);
+
+console.log(`Message: "${message}"`);
